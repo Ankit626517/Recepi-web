cea9b8a09d47158a3bbcb4e30da68ca0af7249e0
Ankit Rathor <ankitrathore48132@gmail.com>
2025-05-11 23:39:31 +0530

    Add heading

--- src/Component/MainSearchBar.jsx
+++ src/Component/MainSearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import MealCard from './MealCard';
-
+import { motion } from "framer-motion";
 function MainSearchBar() {
     const [data, setData] = useState([]);
     const [search, setSearch] = useState("cake");
@@ -21,7 +21,30 @@ function MainSearchBar() {
         <>
      
             <div className=" bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900  p-4">
-           
+                <div className="w-full bg-gradient-to-r from-gray-700 via-blue-950 to-gray-500 py-10 rounded-xl shadow-xl">
+      <div className="max-w-3xl mx-auto text-center px-4">
+        <motion.h1
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="text-4xl sm:text-5xl font-extrabold text-white drop-shadow-md"
+        >
+          Search Your Favorite Dish 🍽️
+        </motion.h1>
+        <motion.p
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.4, duration: 0.8 }}
+          className="text-lg sm:text-xl text-white mt-4 font-medium"
+        >
+          Explore thousands of delicious recipes with ingredients,
+          instructions, and images. Just type the dish name and get started!
+        </motion.p>
+      </div>
+    </div>                                           
+      <br/>
+      <br/>
+      <br/>
                 <form className="max-w-md mx-auto" action="javascript:void(0)" onSubmit={myFun}>
                     <div className="relative">
                         <input
